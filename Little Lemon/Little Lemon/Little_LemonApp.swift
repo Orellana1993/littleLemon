9e90a1bbe19104e154ad8c894d7420c5ac3dd6d6
carlos orellana <orellanacarlos362@gmail.com>
2024-05-18 19:17:04 -0700

    message

---
+++ Little Lemon/Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by carlos orellana on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Little_LemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
